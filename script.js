859a80492af2a125ea983bca4ef0d1210de56f72
NARVS1999 <johnpaulnarvasa6@gmail.com>
2021-07-06 21:28:01 +0800

    javascript complete function

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let computerValue1 = "rock";
 let computerValue2 = "paper";
 let computerValue3 = "scissors";
-let computerSelection = computerPlay();
-let playerSelection = prompt("Write rock/paper/scissors: ");
-console.log("your choice: "  + playerSelection);
 let playerWin = "You win the game!"
-let playerLoss = "YOu loss the game!";
+let playerLoss = "You loss the game!";
 let playerTie = "This game is a tie";
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
 
@@ -27,23 +28,32 @@ function computerPlay() {
 
 
 function playRound (computerSelection, playerSelection) {
+    playerSelection = prompt("Write rock/paper/scissors: ");
+    computerSelection = computerPlay();
+    console.log("your choice: "  + playerSelection);
     if (playerSelection == "rock" && computerSelection == computerValue3) {
         console.log(playerWin);
+        ++playerScore;
         return  playerWin;
     } else if (playerSelection == "rock" && computerSelection == computerValue2) {
         console.log(playerLoss);
+        ++computerScore;
         return playerLoss;
     } else if (playerSelection == "paper" && computerSelection == computerValue1) {
         console.log(playerWin);
+        ++playerScore;
         return playerWin; 
     } else if (playerSelection == "paper" && computerSelection == computerValue3) {
         console.log(playerLoss);
+        ++computerScore;
         return playerLoss;
     } else if (playerSelection == "scissors" && computerSelection == computerValue2) {
         console.log(playerWin);
+        ++playerScore;
         return playerWin;
     } else if (playerSelection == "scissors" && computerSelection == computerValue1) {
         console.log(playerLoss);
+        ++computerScore;
         return playerLoss;
     } else {
         console.log(playerTie);
@@ -51,8 +61,21 @@ function playRound (computerSelection, playerSelection) {
     }
 }
 
+
 function game() {
+
+    for (let i = 1; i <= 5; ++i) {
+        playRound(computerSelection, playerSelection);
+        console.log("Player Score: " + playerScore + " | " + "Computer Score: " + computerScore);
+    }
     
+    if(playerScore > computerScore) {
+        alert("You are a Winner to this game!!!");
+    } else if (playerScore < computerScore) {
+        alert("The Computer is Winner to this game!!!");
+    } else {
+        alert("The score are tie!!! there are no Winner.")
+    }
 }
 
-playRound(computerSelection, playerSelection);
+game();
